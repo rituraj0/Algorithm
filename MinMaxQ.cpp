91f90fe842b14bbd339a771412f09c746d9e7ad4
Rituraj <rituraj.tc@gmail.com>
2014-06-25 20:47:53 +0530

    adding some standard algorithms and datastuctures

---
+++ MinMaxQ.cpp
@@ -0,0 +1,48 @@
+/*
+* A templatized MinMax Queue implementation.
+* Provides O(1) insert, delete, fimnMin and findMax opertions.
+* Assumption : all elements pushed in queue are different. 
+*/
+
+#include<vector>
+using namespace std;
+
+template <class T>
+class MinMaxQueue
+{
+	vector<T> Q, Qmax, Qmin;
+	int qf, qmaxf, qminf, qb, qmaxb, qminb;
+	
+	public :
+		
+		MinMaxQueue(int N)
+		{
+			qf = qb = 0;       Q.resize(N);
+			qmaxf = qmaxb = 0; Qmax.resize(N);
+			qminf = qminb = 0; Qmin.resize(N);	
+		}
+		
+		void push(T v)
+		{
+			while( qmaxf < qmaxb && Qmax[qmaxb -1] < v)	qmaxb--;
+			while( qminf < qminb && Qmin[qminb -1] > v)	qminb--;
+			
+			Q[qb++] = v;
+			Qmax[qmaxb++] = v;
+			Qmin[qminb++] = v;
+		}
+		
+		T pop()
+		{
+			T v = Q[qf++];
+			if (v == Qmax[qmaxf])	qmaxf ++;
+			if (v == Qmin[qminf])	qminf ++;
+			return v;
+		}
+
+		T front()   {  return Q[qf];		}
+		T getMax()  {  return Qmax[qmaxf];	}
+		T getMin()  {  return Qmin[qminf];	}
+		
+		int size()  {  return qb - qf;		}
+};
